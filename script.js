a7b367222b66d33eae7f7320f37426bb0e9f6af1
JeffCoplin <jeffc@JeffCoplin>
2025-06-16 23:19:25 -0400

    Restricted Textarea

---
+++ script.js
@@ -0,0 +1,30 @@
+function contarCaracteres() {
+  const textarea = document.getElementById('miTextarea');
+  const contador = document.getElementById('contador');
+  
+
+  contador.textContent = textarea.value.length;
+
+  if (textarea.value.length === 250){
+    contador.style.color = 'red';
+    textarea.style.borderColor = 'red';
+  }
+
+  else if(textarea.value.length === 0) {
+    contador.style.color = 'black';
+  }
+}
+
+document.getElementById('miTextarea').addEventListener('keydown', function(event) {
+  const contador = document.getElementById('contador');
+    const textarea = document.getElementById('miTextarea');
+
+  if (event.key === 'Enter') {
+    contador.style.color = 'red';
+    textarea.style.borderColor = 'red';
+  }
+  else if (event.key === 'Backspace' || event.key === 'Delete') {
+    contador.style.color = 'black';
+    textarea.style.borderColor = 'black';
+  }
+});
